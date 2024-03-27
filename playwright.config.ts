5e332eef9ed43d81f7a4ee865381c793c081c040
olehhutsuliak <olegucul@gmail.com>
2024-03-28 00:09:21 +0100

    update configs

--- playwright.config.ts
+++ playwright.config.ts
@@ -10,17 +10,12 @@ export const AUTHENTICATED_USER_STATE = path.join(process.cwd(), 'playwright/.au
 
 export default defineConfig({
   testDir: './src/tests',
-  /* Run tests in files in parallel */
   fullyParallel: true,
-  /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
-  /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [['list'], ['html']],
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     trace: 'on-first-retry',
   },
